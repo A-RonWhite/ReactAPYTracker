013aaa0e7b46663df57e5b851f8a66c006bda471
aaronwhite <aaronwhite@itg.co.uk>
2022-03-21 23:16:15 +0000

    style amends and fetch tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -5,65 +5,76 @@ import franciumLogo from "./images/francium-logo.png";
 import tulipLogo from "./images/tulip-logo.svg";
 import "./App.css";
 
-interface currAPY {
+interface APYObject {
   basis: number;
   francium: number;
   tulip: number;
 }
 
 function App() {
-  const [apy, setAPY] = useState<Partial<currAPY>>({});
-  const [totalAPY, setTotalAPY] = useState<Partial<any>>({
-    totalAvg: {
-      basis: 0,
-      francium: 0,
-      tulip: 0,
-    },
+  const [apy, setAPY] = useState<APYObject>({
+    basis: 0,
+    francium: 0,
+    tulip: 0,
   });
-  const [monthlyAPY, setMonthlyAPY] = useState<Partial<any>>({
-    monthAvg: {
-      basis: 0,
-      francium: 0,
-      tulip: 0,
-    },
+  const [totalAPY, setTotalAPY] = useState<APYObject>({
+    basis: 0,
+    francium: 0,
+    tulip: 0,
+  });
+  const [monthlyAPY, setMonthlyAPY] = useState<APYObject>({
+    basis: 0,
+    francium: 0,
+    tulip: 0,
   });
 
   useEffect(() => {
     fetchAPY();
     //call api every 2 and a half mins
-    setInterval(() => fetchAPY(), 150000);
+    const apyInterval = setInterval(() => fetchAPY(), 150000);
 
     fetchAverageAPY();
-    setInterval(() => fetchAverageAPY(), 600000);
+    const avgInternal = setInterval(() => fetchAverageAPY(), 600000);
+
+    return () => {
+      clearInterval(apyInterval);
+      clearInterval(avgInternal);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const fetchAPY = () => {
-    fetch(
-      `https://react-api-feed.herokuapp.com/apy?API_KEY=${process.env.REACT_APP_APY_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data !== undefined || null) {
-          console.log(data);
-          setAPY(data);
-          console.log(apy);
-        }
-      });
+  const fetchAPY = async () => {
+    console.log("fetching APY...");
+
+    try {
+      const res = await fetch(
+        `https://react-api-feed.herokuapp.com/apy?API_KEY=${process.env.REACT_APP_APY_API_KEY}`
+      );
+      const data = await res.json();
+      if (data !== undefined || null) {
+        setAPY(data);
+        console.log(apy);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const fetchAverageAPY = () => {
-    fetch(
-      `https://react-api-feed.herokuapp.com/avg-apy?API_KEY=${process.env.REACT_APP_APY_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.length !== 0) {
-          setTotalAPY(data.totalAvg);
-          setMonthlyAPY(data.monthAvg);
-        }
-      });
+  const fetchAverageAPY = async () => {
+    console.log("fetching Average APYs...");
+    try {
+      const res = await fetch(
+        `https://react-api-feed.herokuapp.com/avg-apy?API_KEY=${process.env.REACT_APP_APY_API_KEY}`
+      );
+      const data = await res.json();
+      console.log(data);
+      if (Object.keys(data).length !== 0) {
+        setTotalAPY(data.totalAvg);
+        setMonthlyAPY(data.monthAvg);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -101,6 +112,9 @@ function App() {
           backgroundColor={"linear-gradient(315deg,#001426,#042e4f)"}
         />
       </main>
+      <p className="start__date">
+        *The starting date for the total APY is from March 13th 2022
+      </p>
     </div>
   );
 }
